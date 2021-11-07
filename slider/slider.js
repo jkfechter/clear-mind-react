db545fb29f952d9452b24537283bbb2c872370eb
jkfechter <jk.fechter@gmail.com>
2021-11-07 08:20:20 -0800

    slider w1
    slider slides along a track.
    some color and font  to make it less unbearable to look at.

--- slider/slider.js
+++ slider/slider.js
@@ -1,17 +1,46 @@
-let dragging = false;
-let sliderOffset = 0;
-const sliderElement = document.getElementsById('slider');
-const trackElement = document.getElementsById('slideTrack');
+// Make DIV element (id slider) draggable:
+dragElement(document.getElementById("sliderKnob"));
 
-slider.addEventListener('mousedown', function(e) {
-    dragging = true;
-});
-slider.addEventListener('mouseup', function(e) {
-    dragging = false;
-});
-window.addEventListener('mousemove', function(e) {
-  if (dragging) {
-      let offset = e.movementY
-      slider.style.transform = `transformY${offset}px`
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+  if (document.getElementById('slider')) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById('slider').onmousedown = dragMouseDown;
+    
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+    console.log(pos2)
   }
-});
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    // pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    // pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    // pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    // elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
